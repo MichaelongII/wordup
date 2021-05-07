bbf6fee714262a33cd3cce726aabfccc6f57999b
MichaeLong <mrl@ualberta.ca>
2021-05-06 23:19:32 -0600

    clean up code and write comments

--- src/db_utils.py
+++ src/db_utils.py
@@ -3,54 +3,80 @@ import click
 
 
 def create_db(key):
+    '''
+    Creates an encrypted sqleet database and adds the pw table
+
+    Parameter - key: string, key to encrypted db
+    '''
     con = sqleet.connect('wordup.db')
     cur = con.cursor()
     cur.execute("PRAGMA key=':key'", {"key": key})
-    cur.execute("create table pass (name, password)")
+    cur.execute("create table pw (name, password)")
     con.commit()
     con.close()
-
     click.echo("database initialized succesfully")
 
 
 def add_pw_to_db(key, password, name):
+    '''
+    Adds a password to the database.
+
+    Parameters
+        - key: string, key to encrypted db
+        - password: string, the password to add
+        - name: string, the name to add
+    '''
     con = sqleet.connect('wordup.db')
     cur = con.cursor()
     cur.execute("PRAGMA key=':key'", {"key": key})
 
-    cur.execute("""SELECT name FROM pass WHERE name=:name""", {"name": name})
+    cur.execute("""SELECT name FROM pw WHERE name=:name""", {"name": name})
     if cur.fetchone():
         con.commit()
         con.close()
         raise DuplicateNameAdd
 
     else:
-        cur.execute("insert into pass values (?, ?)", (name, password))
+        cur.execute("insert into pw values (?, ?)", (name, password))
         con.commit()
         con.close()
 
-def delete_pw_from_db(key, name, username=None):
+def delete_pw_from_db(key, name):
+    '''
+    Deletes a password from the database.
+
+    Parameters
+        - key: string, key to encrypted db
+        - name: string, the name to delete
+    '''
     con = sqleet.connect('wordup.db')
     cur = con.cursor()
     cur.execute("PRAGMA key=':key'", {"key": key})
 
-    cur.execute("""SELECT name FROM pass WHERE name=:name""", {"name": name})
+    cur.execute("""SELECT name FROM pw WHERE name=:name""", {"name": name})
     if cur.fetchone():
         con.commit()
         con.close()
         raise DeleteNonExistentRecord
 
     else:
-        cur.execute("DELETE FROM pass WHERE name=:name""", {"name": name})
+        cur.execute("DELETE FROM pw WHERE name=:name""", {"name": name})
         con.commit()
         con.close()
 
 def get_pw_from_db_by_name(key, name):
+    '''
+    Fetches a password from the database.
+
+    Parameters
+        - key: string, key to encrypted db
+        - name: string, the name to fetch
+    '''
     con = sqleet.connect('wordup.db')
     cur = con.cursor()
     cur.execute("PRAGMA key=':key'", {"key": key})
 
-    cur.execute("""SELECT password FROM pass WHERE name=:name""", {"name": name})
+    cur.execute("""SELECT password FROM pw WHERE name=:name""", {"name": name})
     result = cur.fetchone()
     if result:
         con.commit()
@@ -60,14 +86,12 @@ def get_pw_from_db_by_name(key, name):
         raise GetNonExistentRecord
 
 
+# Custom exceptions
 class DuplicateNameAdd(Exception):
-    """Raised when the input value is too small"""
     pass
 
 class DeleteNonExistentRecord(Exception):
-    """Raised when the input value is too small"""
     pass
 
 class GetNonExistentRecord(Exception):
-    """Raised when the input value is too small"""
     pass
---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import click
+import os
+from functools import update_wrapper
+
+def validate_name(ctx, param, value):
+    if not value.isalnum():
+        raise click.BadParameter('names must be alphanumeric')
+    else:
+        return value
+
+def print_general_error():
+    msg = ('Could not add password, please ensure you entered your wordup '
+            'password correctly.')
+    click.echo(click.style(msg, fg='red'))
