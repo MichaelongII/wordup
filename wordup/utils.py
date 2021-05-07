a2378d0aa51488653daf53d69398f891db1bd5d6
MichaeLong <mrl@ualberta.ca>
2021-05-06 22:50:59 -0600

    commands interface

--- wordup/utils.py
+++ wordup/utils.py
@@ -1,4 +1,6 @@
 import click
+import os
+from functools import update_wrapper
 
 def generate_password():
     pass
@@ -8,3 +10,31 @@ def validate_name(ctx, param, value):
         raise click.BadParameter('names must be alphanumeric')
     else:
         return value
+
+def print_help_msg(command):
+    with click.Context(command) as ctx:
+        click.echo(command.get_help(ctx))
+
+def print_general_error():
+    msg = ('Could not add password, please ensure you entered your wordup '
+            'password correctly.')
+    click.echo(click.style(msg, fg='red'))
+
+def init_required(f):
+    @click.pass_context
+    def new_func(ctx, *args, **kwargs):
+        return ctx.invoke(f, ctx.obj, *args, **kwargs)
+    if os.path.exists('wordup.db'):
+        return update_wrapper(new_func, f)
+    else:
+        raise NoDbException
+
+
+def is_db_missing():
+    if not os.path.exists('wordup.db'):
+        msg = ("Your password db is not set up yet, run 'wordup init'"
+                'to set your wordup password.')
+        click.echo(click.style(msg, fg='red'))
+
+class NoDbException(Exception):
+    pass
--- wordup/wordup.py
+++ wordup/wordup.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 import click
 import clipboard
+import os.path
 from db_utils import *
-from utils import generate_password, validate_name
+from utils import generate_password, validate_name, print_general_error
 from hibp_api import check_pw_hibp
 
 @click.command()
@@ -11,7 +13,7 @@ from hibp_api import check_pw_hibp
 @click.argument('name', callback=validate_name)
 def add(key, password, name):
     '''
-    Adds a password to the db with a 'name'.
+    [NAME] - Adds a password to the db with a corresponding NAME.
 
     Password is checked against the HIBP pwned passwords API and will
     only be added if it is not present in their list of leaked passwords.
@@ -23,60 +25,114 @@ def add(key, password, name):
     '''
     try:
         if not check_pw_hibp(password):
-            msg = ('The password you entered has been leaked in a data breach '
+            msg = ('\nThe password you entered has been leaked in a data breach '
                     'and therefore is insecure, please enter a different '
                     'password.\nFor more information see '
                     'https://haveibeenpwned.com/Passwords')
             click.echo(click.style(msg, fg='red'))
             return
-        # add_pw_to_db(key, password, name)
-        click.echo('Password for {} successfully added'.format(name))
-    except Exception:
-        msg = ('Could not add password, please ensure you entered your wordup '
-                'password correctly.')
+        add_pw_to_db(key, password, name)
+        click.echo("\nPassword '{}' successfully added".format(name))
+
+    except DuplicateNameAdd:
+        msg = ('That name has already been added, please add this password'
+                'under a different name.')
         click.echo(click.style(msg, fg='red'))
 
+    # except Exception as e:
+    #     print_general_error()
+    #     print(e)
 
 @click.command()
 @click.option('--key', prompt="wordup password", hide_input=True)
 @click.argument('name', callback=validate_name)
 def get(key, name):
+    '''
+    [NAME] - Places the password with NAME into the clipboard.
+
+    Parameters:
+        - key: string, key to encrypted db
+        - name: string, name of password to be added
+    '''
     try:
         password = get_pw_from_db_by_name(key, name)
         clipboard.copy(password)
-        click.echo('Password for {} copied to clipboard'.format(name))
-    except Exception as e:
-        click.echo(e.args)
+        click.echo("\nPassword '{}' copied to clipboard".format(name))
+
+    except GetNonExistentRecord:
+        msg = ("\nThere's no password stored under that name")
+        click.echo(click.style(msg, fg='red'))
+
+    # except Exception:
+    #     print_general_error()
 
 
 @click.command()
 @click.option('--key', prompt="wordup password", hide_input=True)
 @click.argument('name', callback=validate_name)
 def delete(key, name):
+    '''
+    [NAME] - Deletes a password from the db with a corresponding NAME.
+
+    Parameters:
+        - key: string, key to encrypted db
+        - name: string, name of password to be added
+    '''
     try:
-        # delete_pw_from_db(key, name)
-        click.echo('Password for {} deleted'.format(name))
-    except Exception as e:
-        click.echo(e.args)
+        delete_pw_from_db(key, name)
+        click.echo('\nPassword for {} deleted'.format(name))
+
+    except DeleteNonExistentRecord:
+        msg = ("\nThere's no password stored under that name")
+        click.echo(click.style(msg, fg='red'))
+
+    # except Exception:
+    #     print_general_error()
 
 
 @click.command()
 @click.option('-p', '--password', 'password', prompt="password to set",
                 hide_input=True, confirmation_prompt=True)
 def check(password):
+    '''
+    [PASSWORD] - Checks if a PASSWORD was in a data breach.
+
+    Parameters:
+        - key: string, key to encrypted db
+        - name: string, name of password to be added
+    '''
     click.echo('\nChecking if password has been comprimised...')
     if check_pw_hibp(password):
-        click.echo("Password has not been pwned!")
+        click.echo("\nPassword has not been pwned!")
+
     else:
-        msg = ("Password is the haveibeenpwned pwned passwords database! "
+        msg = ("\nPassword is the haveibeenpwned pwned passwords database! "
                     "Don't use that password!")
         click.echo(click.style(msg, fg='red'))
 
 
 @click.command()
 def clear():
+    '''
+    Clears the contents of the clipboard.
+    '''
     clipboard.copy("")
     click.echo('Clipboard cleared')
+
+
+@click.command()
+@click.option('--key', prompt="set your wordup password", hide_input=True,
+                confirmation_prompt=True)
+def init(key):
+    '''
+    Creates an encrypted database for your passwords.
+    '''
+    create_db(key)
+    click.echo("\nwordup password set")
+
+@click.group()
+def first_run():
+    pass
 
 
 @click.group()
@@ -87,9 +143,13 @@ def cli():
 cli.add_command(add)
 cli.add_command(get)
 cli.add_command(delete)
-cli.add_command(clear)
 cli.add_command(check)
+cli.add_command(clear)
+first_run.add_command(init)
 
 
 if __name__ == '__main__':
-    cli()
+    if os.path.exists('wordup.db'):
+        cli()
+    else:
+        first_run()
